b2689f9fca46a99aaa7ddd2a4e470bd91d295efd
Zlobarq <georgi.atanasov@uni-weimar.de>
2017-05-31 02:07:26 +0200

    vsichko

--- heapsort2.cpp
+++ heapsort2.cpp
@@ -7,19 +7,15 @@ void max_heapify(string &a,int index,int size)
 {
     int left = 2*index + 1;
     int right = 2*index + 2;
-    int smallest=0;
     if (left<size && a[left]<a[index])
     {
 	//cout <<a[left]<< "is smaller than"<<a[index]<<" \n";
-    smallest=left;
 	auto temp = a[index];
 	a[index]=a[left];
 	a[left]=temp;
-	max_heapify(a,smallest,size);
 	}
     if (right<size && a[right]<a[index])
 	{
-	smallest=right;
 	//cout <<a[right]<< "is smaller than"<<a[index]<<" \n";
 	auto temp = a[index];
 	a[index]=a[right];
@@ -51,7 +47,7 @@ for (int i=a.size()-1;i>=0;i--)
 	a[i]=newroot;
 	newsize-=1;
 	create_heap(a,newsize);
-	cout<<newroot<<"\n";
+	//cout<<newroot<<"\n";
 }
 }
 
